6599b3adf7fc4cf73ba110c8bed71255d0903858
Gowtham vulluri <gowthamkumarvulluri@gmail.com>
2021-07-10 21:44:15 +0530

    daa

---
+++ qsort1.py
@@ -0,0 +1,66 @@
+import random
+import timeit
+import math
+import matplotlib.pyplot as pl
+def partition(arr, beg, end):
+    pivot=arr[beg]
+    left=beg+1
+    right=end
+    global comp
+    while(1):
+        while left<=right and arr[left]<=pivot:
+            left+=1
+            comp+=1
+        while left<=right and arr[right]>=pivot:
+            right-=1
+            comp+=1
+        if(left<right):
+            arr[left], arr[right]=arr[right], arr[left]
+            comp+=1
+        else:
+            comp+=1
+            break
+    arr[beg], arr[right]=arr[right], arr[beg]
+    return right
+
+def quickSort(arr, beg, end):
+    if beg<end:
+        p=partition(arr, beg, end)
+        quickSort(arr, beg,p-1)
+        quickSort(arr,p+1, end)
+timeList=[]
+compList=[]
+for i in range (200):
+    comp=1
+    arr=[]
+    for j in range(0,i):
+        n = random.randint(1,100)
+        arr.append(n)
+    beg = timeit.default_timer()
+    quickSort(arr,0,i-1)
+    end = timeit.default_timer()
+    total=end-beg
+    timeList.append(total)
+    compList.append(comp)
+timelist = [x * 1000 for x in timeList]
+avgtime=0
+for i in timeList:
+    avgtime+=i
+avgtime/=200
+n=[*range(1, 201, 1)]
+nn=[]
+for x in n:
+    nn.append(math.pow(x,2))
+nnn=[]
+for x in n:
+    nnn.append(x*math.log(x,2))
+print("Time required:", avgtime)
+pl.plot(compList, n, color='green', linewidth=5, label='Quicksort time')
+pl.plot(nn, n, color='black', linewidth=3, label='n^2')
+pl.plot(nnn,n, color='red', linewidth=3, label='nlogn')
+pl.title('Quick Sort Running time')
+pl.xlabel('value of time')
+pl.ylabel('value of n')
+pl.legend()
+pl.show()
+
